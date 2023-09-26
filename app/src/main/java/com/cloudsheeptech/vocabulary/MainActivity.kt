f8215a73f9ad69a706bcbb40d4cb9f53b0981157
Justus <beek@in.tum.de>
2023-09-26 09:27:36 +0200

    Started implementing network request

--- app/src/main/java/com/cloudsheeptech/vocabulary/MainActivity.kt
+++ app/src/main/java/com/cloudsheeptech/vocabulary/MainActivity.kt
@@ -1,11 +1,14 @@
 package com.cloudsheeptech.vocabulary
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import com.cloudsheeptech.vocabulary.databinding.ActivityMainBinding
-import com.cloudsheeptech.vocabulary.databinding.FragmentLearningBinding
+import java.io.BufferedInputStream
+import java.lang.Exception
+import java.net.URL
 
 class MainActivity : AppCompatActivity() {
 
@@ -15,7 +18,21 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
 
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+    }
 
-
+    private fun requestVocabulary() {
+        Log.i("MainActivity", "Request started...")
+        val url = URL("http://localhost:50000/v1/words")
+//        val url = URL("https://vocabulary.cloudsheeptech.com/v1/words")
+        val urlConnection = url.openConnection()
+        try {
+            val income = BufferedInputStream(urlConnection.getInputStream())
+            val data = income.readBytes()
+            val string = data.decodeToString()
+            income.close()
+            Log.i("MainActivity", "Got data: $string")
+        } catch (ex : Exception) {
+            Log.i("MainActivity", "Failed to make request")
+        }
     }
 }
--- app/src/main/java/com/cloudsheeptech/vocabulary/learning/LearningViewModel.kt
+++ app/src/main/java/com/cloudsheeptech/vocabulary/learning/LearningViewModel.kt
@@ -1,8 +1,12 @@
 package com.cloudsheeptech.vocabulary.learning
 
+import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import java.io.BufferedInputStream
+import java.lang.Exception
+import java.net.URL
 
 class LearningViewModel : ViewModel() {
     private val _learningVocab = MutableLiveData<String>()
@@ -16,6 +20,21 @@ class LearningViewModel : ViewModel() {
     init {
         _learningVocab.value = "Learning"
         _translateVocab.value = "Lernen"
+    }
+
+    fun requestVocabulary() {
+        val url = URL("http://localhost:50000/v1/words")
+//        val url = URL("https://vocabulary.cloudsheeptech.com/v1/words")
+        val urlConnection = url.openConnection()
+        try {
+            val income = BufferedInputStream(urlConnection.getInputStream())
+            val data = income.readBytes()
+            val string = data.decodeToString()
+            income.close()
+            Log.i("LearningViewModel", "Got data: $string")
+        } catch (ex : Exception) {
+            Log.i("LearningViewModel", "Failed to make request")
+        }
     }
 
 }

ac580cf37211d0a24b742d82b45aebd95f1a0dd8
Justus von der Beek <justusvonderbeek@gmail.com>
2023-09-28 21:33:03 +0200

    Include new vocab showing

--- app/src/main/java/com/cloudsheeptech/vocabulary/learning/LearningViewModel.kt
+++ app/src/main/java/com/cloudsheeptech/vocabulary/learning/LearningViewModel.kt
@@ -4,12 +4,26 @@ import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.cloudsheeptech.vocabulary.data.Vocabulary
+import io.ktor.client.HttpClient
+import io.ktor.client.request.get
+import io.ktor.client.request.request
+import io.ktor.client.statement.HttpResponse
+import io.ktor.http.HttpMethod
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.launch
 import java.io.BufferedInputStream
 import java.lang.Exception
 import java.net.URL
 
 class LearningViewModel : ViewModel() {
-    private val _learningVocab = MutableLiveData<String>()
+
+    private val job = Job()
+    private val vmScope = CoroutineScope(Dispatchers.Main + job)
+
+    private var _learningVocab = MutableLiveData<String>()
     val learningVocabulary : LiveData<String>
         get() = _learningVocab
 
@@ -17,24 +31,24 @@ class LearningViewModel : ViewModel() {
     val translateVocabulary : LiveData<String>
         get() = _translateVocab
 
+    private val vocabulary = Vocabulary()
+
     init {
         _learningVocab.value = "Learning"
         _translateVocab.value = "Lernen"
     }
 
     fun requestVocabulary() {
-        val url = URL("http://localhost:50000/v1/words")
-//        val url = URL("https://vocabulary.cloudsheeptech.com/v1/words")
-        val urlConnection = url.openConnection()
-        try {
-            val income = BufferedInputStream(urlConnection.getInputStream())
-            val data = income.readBytes()
-            val string = data.decodeToString()
-            income.close()
-            Log.i("LearningViewModel", "Got data: $string")
-        } catch (ex : Exception) {
-            Log.i("LearningViewModel", "Failed to make request")
+        vmScope.launch {
+            vocabulary.updateVocabulary()
         }
     }
 
+    fun showNewVocabulary() {
+        Log.i("LearningViewModel", "Pressed new vocab")
+        val next = vocabulary.getNextVocabulary()
+        this._learningVocab.value = next.Vocabulary
+        this._translateVocab.value = next.Translation
+        Log.i("LearningViewModel", "Updated vocab to: ${_learningVocab.value}")
+    }
 }

faf0758332e48714d302d33ac0eb151bcec81067
Justus von der Beek <justusvonderbeek@gmail.com>
2023-09-28 10:14:55 +0200

    Testing all three methods

--- app/src/main/java/com/cloudsheeptech/vocabulary/MainActivity.kt
+++ app/src/main/java/com/cloudsheeptech/vocabulary/MainActivity.kt
@@ -2,23 +2,14 @@ package com.cloudsheeptech.vocabulary
 
 import android.os.Bundle
 import android.util.Log
-import androidx.activity.ComponentActivity
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
+import com.cloudsheeptech.data.Vocabulary
 import com.cloudsheeptech.vocabulary.databinding.ActivityMainBinding
-import io.ktor.client.HttpClient
-import io.ktor.client.request.get
-import io.ktor.client.statement.HttpResponse
-import io.ktor.client.statement.bodyAsText
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.Job
 import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
-import java.io.BufferedInputStream
-import java.lang.Exception
-import java.net.URL
-import kotlin.concurrent.thread
 
 class MainActivity : AppCompatActivity() {
 
@@ -35,13 +26,10 @@ class MainActivity : AppCompatActivity() {
         val job = Job()
         val scope = CoroutineScope(Dispatchers.Main + job)
         scope.launch {
-            withContext(Dispatchers.IO) {
-                val client = HttpClient()
-                val response : HttpResponse = client.get("http://10.0.2.2:50000/words")
-                println(response)
-                println(response.bodyAsText(Charsets.UTF_8))
-                client.close()
-            }
+            val vocab = Vocabulary()
+            vocab.updateVocabulary()
+            vocab.postVocabulary()
+            vocab.getVocabularyItem(10)
         }
     }
 }

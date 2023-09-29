73a0df85a553f5a8d8459c350ef142a30d9412c7
Justus von der Beek <justusvonderbeek@gmail.com>
2023-09-29 13:17:28 +0200

    Fixed reusing http client

--- app/src/main/java/com/cloudsheeptech/vocabulary/data/Vocabulary.kt
+++ app/src/main/java/com/cloudsheeptech/vocabulary/data/Vocabulary.kt
@@ -1,8 +1,12 @@
 package com.cloudsheeptech.vocabulary.data
 
 import android.util.Log
+import android.widget.Toast
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import io.ktor.client.HttpClient
 import io.ktor.client.call.body
+import io.ktor.client.engine.okhttp.OkHttp
 import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
 import io.ktor.client.request.HttpRequestBuilder
 import io.ktor.client.request.get
@@ -16,6 +20,7 @@ import io.ktor.serialization.kotlinx.json.*
 import io.ktor.util.Identity.encode
 import kotlinx.serialization.encodeToString
 import kotlinx.serialization.json.Json
+import java.lang.Exception
 
 class Vocabulary {
 
@@ -26,6 +31,8 @@ class Vocabulary {
 
     private var wordIndex = 0
 
+    private lateinit var client : HttpClient
+
     fun getNextVocabulary() : Word {
         if (_vocabulary.isEmpty()) {
             return Word(-1, "Null", "Null")
@@ -33,48 +40,70 @@ class Vocabulary {
         return vocabulary[wordIndex++ % vocabulary.size]
     }
 
-    suspend fun updateVocabulary() {
+    private suspend fun initClient() {
         withContext(Dispatchers.IO) {
-            val client = HttpClient() {
+            client = HttpClient(OkHttp) {
+                engine {
+                    config {
+                        hostnameVerifier {
+                                _, _ -> true
+                        }
+                    }
+                }
                 install(ContentNegotiation) {
                     json()
                 }
             }
-            val response : HttpResponse = client.get("http://10.0.2.2:50000/words")
-            println("Body:\n${response.bodyAsText(Charsets.UTF_8)}")
-            val bdy = response.body<List<Word>>()
-            _vocabulary.addAll(bdy)
+        }
+    }
+
+    suspend fun updateVocabulary() {
+        val init = this::client.isInitialized
+        withContext(Dispatchers.IO) {
+            if (!init) {
+                initClient()
+            }
+            try {
+                val response : HttpResponse = client.get("https://vocabulary.cloudsheeptech.com:50002/words")
+                println("Body:\n${response.bodyAsText(Charsets.UTF_8)}")
+                val bdy = response.body<List<Word>>()
+                _vocabulary.addAll(bdy)
 //            Log.i("Vocabulary", "Updated list to $_vocabulary")
-            client.close()
+                Log.i("Vocabulary", "Update successful")
+            } catch (ex : Exception) {
+                Log.e("Vocabulary", "Failed to make request:\n$ex")
+            }
         }
     }
 
     suspend fun getVocabularyItem(id : Int) {
+        val init = this::client.isInitialized
         withContext(Dispatchers.IO) {
-            val client = HttpClient() {
-                install(ContentNegotiation) {
-                    json()
-                }
+            if (!init)
+                initClient()
+            try {
+                val response : HttpResponse = client.get("https://vocabulary.cloudsheeptech.com:50002/words/$id")
+                println("Body:\n${response.bodyAsText()}")
+            } catch (ex : Exception) {
+                Log.e("Vocabulary", "Failed to get item:\n$ex")
             }
-            val response : HttpResponse = client.get("http://10.0.2.2:50000/words/$id")
-            println("Body:\n${response.bodyAsText()}")
-            client.close()
         }
     }
 
     suspend fun postVocabulary(vocab : String, translation : String) {
+        val init = this::client.isInitialized
         withContext(Dispatchers.IO) {
-            val client = HttpClient() {
-                install(ContentNegotiation) {
-                    json()
-                }
-            }
+            if (!init)
+                initClient()
             val word = Word(vocabulary.size, vocab, translation)
-            val rawWord = Json.encodeToString(word)
-            val response : HttpResponse = client.post("http://10.0.2.2:50000/words") {
-                setBody(rawWord)
+            try {
+                val rawWord = Json.encodeToString(word)
+                val response : HttpResponse = client.post("https://vocabulary.cloudsheeptech.com:50002/words") {
+                    setBody(rawWord)
+                }
+            } catch (ex : Exception) {
+                Log.e("Vocabulary", "Failed to post item:\n$ex")
             }
-            client.close()
         }
     }
 }

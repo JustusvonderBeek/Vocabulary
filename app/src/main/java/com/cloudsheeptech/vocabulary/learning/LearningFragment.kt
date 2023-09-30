a335b84055eca624b6403fa2a7bdd6996aba9253
Justus von der Beek <justusvonderbeek@gmail.com>
2023-09-30 18:21:58 +0200

    Started enabling editing word

--- app/src/main/java/com/cloudsheeptech/vocabulary/learning/LearningFragment.kt
+++ app/src/main/java/com/cloudsheeptech/vocabulary/learning/LearningFragment.kt
@@ -2,11 +2,13 @@ package com.cloudsheeptech.vocabulary.learning
 
 import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
+import android.text.InputType
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.Observer
 import com.cloudsheeptech.vocabulary.R
 import com.cloudsheeptech.vocabulary.databinding.FragmentLearningBinding
 
@@ -24,6 +26,19 @@ class LearningFragment : Fragment() {
         viewModel = ViewModelProvider(requireActivity(), viewModelFactory)[LearningViewModel::class.java]
         binding.learningVM = viewModel
         binding.lifecycleOwner = this
+
+        viewModel.editingWord.observe(viewLifecycleOwner, Observer {
+            if (it) {
+                binding.vocabView.inputType = InputType.TYPE_CLASS_TEXT
+                binding.vocabView.focusable = View.FOCUSABLE
+                binding.translateView.inputType = InputType.TYPE_CLASS_TEXT
+
+                binding.editButton.text = "Update word"
+            } else {
+                binding.vocabView.inputType = InputType.TYPE_NULL
+                binding.translateView.inputType = InputType.TYPE_NULL
+            }
+        })
 
         return binding.root
     }
--- app/src/main/java/com/cloudsheeptech/vocabulary/learning/LearningViewModel.kt
+++ app/src/main/java/com/cloudsheeptech/vocabulary/learning/LearningViewModel.kt
@@ -1,10 +1,14 @@
 package com.cloudsheeptech.vocabulary.learning
 
 import android.util.Log
+import android.widget.Toast
+import androidx.databinding.Observable
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModel
 import com.cloudsheeptech.vocabulary.data.Vocabulary
+import com.cloudsheeptech.vocabulary.data.Word
 import io.ktor.client.HttpClient
 import io.ktor.client.request.get
 import io.ktor.client.request.request
@@ -31,11 +35,21 @@ class LearningViewModel : ViewModel() {
     val translateVocabulary : LiveData<String>
         get() = _translateVocab
 
+    private val _progress = MutableLiveData<String>()
+    val progress : LiveData<String>
+        get() = _progress
+
+    private val _editToggle = MutableLiveData<Boolean>()
+    val editingWord : LiveData<Boolean>
+        get() = _editToggle
+
     private val vocabulary = Vocabulary()
 
     init {
         _learningVocab.value = "Learning"
         _translateVocab.value = "Lernen"
+        _progress.value = "0/0"
+        _editToggle.value = false
     }
 
     fun requestVocabulary() {
@@ -49,6 +63,22 @@ class LearningViewModel : ViewModel() {
         val next = vocabulary.getNextVocabulary()
         this._learningVocab.value = next.Vocabulary
         this._translateVocab.value = next.Translation
+        this._progress.value = (vocabulary.wordIndex + 1).toString() + "/" + vocabulary.size.toString()
         Log.i("LearningViewModel", "Updated vocab to: ${_learningVocab.value}")
+    }
+
+    fun editWord() {
+        Log.i("LearningViewModel", "Switched editing vocab")
+        _editToggle.value = !_editToggle.value!!
+    }
+
+    fun wordEdited() {
+        _editToggle.value = false
+        if (_learningVocab.value == null || _translateVocab.value == null)
+            return
+        val updatedWord = Word(vocabulary.wordIndex, _learningVocab.value!!, _translateVocab.value!!)
+        vmScope.launch {
+            vocabulary.postItemVocabulary(updatedWord)
+        }
     }
 }

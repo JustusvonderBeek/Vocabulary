35be4f6bf4558af3d10603c46be0af7cc6c15b9f
Justus von der Beek <justusvonderbeek@gmail.com>
2023-10-08 17:46:55 +0200

    Write vocabulary to disk

--- app/src/main/java/com/cloudsheeptech/vocabulary/data/Vocabulary.kt
+++ app/src/main/java/com/cloudsheeptech/vocabulary/data/Vocabulary.kt
@@ -13,17 +13,21 @@ import io.ktor.client.request.setBody
 import io.ktor.client.statement.HttpResponse
 import io.ktor.client.statement.bodyAsText
 import io.ktor.http.HttpStatusCode
+import io.ktor.http.set
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 import io.ktor.serialization.kotlinx.json.*
 import kotlinx.serialization.encodeToString
 import kotlinx.serialization.json.Json
+import okhttp3.Credentials
+import okhttp3.Request
 import java.io.File
 import kotlin.Exception
 
-class Vocabulary {
+class Vocabulary(private val vocabularyLocation : File) {
 
     private val baseUrl = "https://vocabulary.cloudsheeptech.com:50002/"
+//    private val baseUrl = "https://10.0.2.2:50002/"
     private var _vocabulary = mutableListOf<Word>()
 
     val vocabulary : List<Word>
@@ -31,22 +35,44 @@ class Vocabulary {
 
     private lateinit var client : HttpClient
 
-    private fun loadVocabularyFromDisk() {
-
+    init {
+        loadVocabularyFromDisk()
     }
 
-    private fun storeVocabularyToDisk() {
-        val file = File(Environment.getDataDirectory(), "vocabulary.json")
-        val writer = file.writer(Charsets.UTF_8)
-        val stringVocab = Json.encodeToString(_vocabulary)
-        writer.write(stringVocab)
-        writer.close()
+    private fun loadVocabularyFromDisk() {
+        try {
+            val file = vocabularyLocation
+            val reader = file.reader(Charsets.UTF_8)
+            val fileContent = reader.readText()
+            reader.close()
+            val wordList = Json.decodeFromString<List<Word>>(fileContent)
+            Log.i("Vocabulary", "Loaded vocabulary with ${wordList.size} words from disk")
+            _vocabulary.addAll(wordList)
+        } catch (ex : Exception) {
+            Log.i("Vocabulary", "Failed to load vocabulary from disk: $ex")
+        }
+    }
+
+    private suspend fun storeVocabularyToDisk() {
+        withContext(Dispatchers.IO) {
+            try {
+                val file = vocabularyLocation
+                val writer = file.writer(Charsets.UTF_8)
+                val stringVocab = Json.encodeToString(_vocabulary)
+                writer.write(stringVocab)
+                writer.close()
+                Log.i("Vocabulary", "Stored vocabulary to disk")
+            } catch (ex : Exception) {
+                Log.i("Vocabulary", "Failed to write vocabulary to disk: $ex")
+            }
+        }
     }
 
     private suspend fun initClient() {
         withContext(Dispatchers.IO) {
             client = HttpClient(OkHttp) {
                 engine {
+                    addInterceptor(AuthenticationInterceptor("eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJhdXRob3JpemVkIjp0cnVlLCJ1c2VySWQiOiJsZXJuZXIifQ.Jg2RA1ctZVb77s8laIEvDmBs0pCu4M-0Mabozqlwnqd2skBYsEVXK0wNJmN8GIiGTzc4JiJt6FzwKkkOTl6EHQ"))
                     config {
                         hostnameVerifier {
                                 _, _ -> true
@@ -67,11 +93,14 @@ class Vocabulary {
                 initClient()
             }
             try {
+//                val getRequest = Request.Builder().url(baseUrl+"words").addHeader("Authorization", credentials)
+//                val response : HttpResponse = client.get { getRequest.get() }
                 val response : HttpResponse = client.get(baseUrl + "words")
                 println("Body:\n${response.bodyAsText(Charsets.UTF_8)}")
                 val bdy = response.body<List<Word>>()
                 _vocabulary.clear()
                 _vocabulary.addAll(bdy)
+                storeVocabularyToDisk()
 //            Log.i("Vocabulary", "Updated list to $_vocabulary")
                 Log.i("Vocabulary", "Update successful")
             } catch (ex : Exception) {
@@ -125,12 +154,18 @@ class Vocabulary {
             if (!init)
                 initClient()
             try {
+                Log.i("Vocabulary", "Got word for modification: $word")
                 val rawWord = Json.encodeToString(word)
                 val response : HttpResponse = client.post(baseUrl + "words/" + word.ID) {
                     setBody(rawWord)
                 }
                 if (response.status != HttpStatusCode.Created) {
                     Log.e("Vocabulary", "Modification of vocabulary not successful")
+                } else {
+                    val bdy = response.body<List<Word>>()
+                    _vocabulary.clear()
+                    _vocabulary.addAll(bdy)
+                    storeVocabularyToDisk()
                 }
                 return@withContext
             } catch (ex : Exception) {

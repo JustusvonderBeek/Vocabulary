538340e35fd3b4be9352216546ca13c6dc3a2d5d
Justus <beek@in.tum.de>
2023-10-13 11:23:34 +0200

    Include loading data

--- app/src/main/java/com/cloudsheeptech/vocabulary/edit/EditFragment.kt
+++ app/src/main/java/com/cloudsheeptech/vocabulary/edit/EditFragment.kt
@@ -1,11 +1,13 @@
 package com.cloudsheeptech.vocabulary.edit
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.cloudsheeptech.vocabulary.R
 import com.cloudsheeptech.vocabulary.data.Vocabulary
@@ -34,6 +36,20 @@ class EditFragment : Fragment() {
         viewModel = ViewModelProvider(requireActivity(), viewModelFactory)[EditViewModel::class.java]
         binding.viewModel = viewModel
         binding.lifecycleOwner = this
+
+        val adapter = WordListItemAdapter(WordListItemAdapter.WordListItemListener { wordId ->
+            Log.i("EditFragment", "Tapped on word with ID $wordId")
+            // TODO: Implement handling
+        })
+
+        binding.vocabList.adapter = adapter
+
+        viewModel.vocabList.observe(viewLifecycleOwner, Observer {
+            it?.let {
+                adapter.submitList(it)
+                adapter.notifyDataSetChanged()
+            }
+        })
 
         return binding.root
     }

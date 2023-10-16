eba6b7e590c5db30af76cddb598121bbce4010d4
Justus <beek@in.tum.de>
2023-10-16 19:28:43 +0200

    Fixed single instance of vocabulary

--- app/src/main/java/com/cloudsheeptech/vocabulary/MainActivity.kt
+++ app/src/main/java/com/cloudsheeptech/vocabulary/MainActivity.kt
@@ -1,14 +1,11 @@
 package com.cloudsheeptech.vocabulary
 
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
-import androidx.lifecycle.viewmodel.viewModelFactory
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.NavigationUI
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.cloudsheeptech.vocabulary.data.Vocabulary
@@ -16,10 +13,6 @@ import com.cloudsheeptech.vocabulary.databinding.ActivityMainBinding
 import com.cloudsheeptech.vocabulary.learning.LearningViewModel
 import com.cloudsheeptech.vocabulary.learning.LearningViewModelFactory
 import com.google.android.material.bottomnavigation.BottomNavigationView
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.Job
-import kotlinx.coroutines.launch
 import java.io.File
 
 class MainActivity : AppCompatActivity() {
@@ -41,7 +34,7 @@ class MainActivity : AppCompatActivity() {
         botNav.setupWithNavController(navController)
 
         val vocabularyFile = File(applicationContext.filesDir, "vocabulary.json")
-        val vocabulary = Vocabulary(vocabularyFile)
+        val vocabulary = Vocabulary.getInstance(vocabularyFile)
         val activityViewModel by viewModels<LearningViewModel> { LearningViewModelFactory(vocabulary) }
         learningViewModel = activityViewModel
     }

d0607a3c8057e37199a9f242ab909e5777d94d7d
Justus <beek@in.tum.de>
2023-10-16 19:28:30 +0200

    Moved functionality in other fragments

--- app/src/main/java/com/cloudsheeptech/vocabulary/learning/LearningFragment.kt
+++ app/src/main/java/com/cloudsheeptech/vocabulary/learning/LearningFragment.kt
@@ -3,69 +3,69 @@ package com.cloudsheeptech.vocabulary.learning
 import android.content.Context
 import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
-import android.text.InputType
 import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
 import android.view.inputmethod.InputMethodManager
+import androidx.core.view.MenuProvider
 import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.Observer
+import androidx.navigation.fragment.findNavController
 import com.cloudsheeptech.vocabulary.R
 import com.cloudsheeptech.vocabulary.data.Vocabulary
 import com.cloudsheeptech.vocabulary.databinding.FragmentLearningBinding
 import java.io.File
 
-class LearningFragment : Fragment() {
+class LearningFragment : Fragment(), MenuProvider {
 
     private lateinit var viewModel: LearningViewModel
     private lateinit var binding : FragmentLearningBinding
+
+    override fun onCreateMenu(menu: Menu, menuInflater: MenuInflater) {
+        menuInflater.inflate(R.menu.learning_dropdown, menu)
+    }
+
+    override fun onMenuItemSelected(menuItem: MenuItem): Boolean {
+        when (menuItem.itemId) {
+            R.id.dd_edit_btn -> {
+                viewModel.editWord()
+                return true
+            }
+            R.id.dd_delete_btn -> {
+                viewModel.removeWord()
+                return true
+            }
+        }
+        return false
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
         binding = DataBindingUtil.inflate(inflater, R.layout.fragment_learning, container, false)
+
+        requireActivity().addMenuProvider(this, viewLifecycleOwner, Lifecycle.State.RESUMED)
+
         val vocabFile = File(requireActivity().applicationContext.filesDir, "vocabulary.json")
-        val viewModelFactory = LearningViewModelFactory(Vocabulary(vocabFile))
+        val viewModelFactory = LearningViewModelFactory(Vocabulary.getInstance(vocabFile))
         viewModel = ViewModelProvider(requireActivity(), viewModelFactory)[LearningViewModel::class.java]
         binding.learningVM = viewModel
         binding.lifecycleOwner = this
 
-        viewModel.editToggle.observe(viewLifecycleOwner, Observer {
-            it.getContentIfNotHandled()?.let {edit ->
-                edit.let {
-                    Log.i("LearningFragment", "Observer changing to focusable")
-                    binding.vocabView.focusable = View.FOCUSABLE
-                    // For whatever reason this API is boolean now?
-                    binding.vocabView.isFocusableInTouchMode = true
-                    binding.translateView.focusable = View.FOCUSABLE
-                    binding.translateView.isFocusableInTouchMode = true
-                    binding.editButton.text = "Update word"
-
-                    // Show keyboard
-                    val imm = requireActivity().getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
-                    imm.showSoftInput(binding.vocabView, InputMethodManager.SHOW_IMPLICIT)
-                }
-            }
-        })
-
-        viewModel.editedToggle.observe(viewLifecycleOwner, Observer {
-            it.getContentIfNotHandled()?.let {edited ->
-                edited.let {
-                    Log.i("LearningFragment", "Observer changing to no focus")
-                    binding.vocabView.focusable = View.NOT_FOCUSABLE
-                    binding.vocabView.isFocusableInTouchMode = false
-                    binding.translateView.focusable = View.NOT_FOCUSABLE
-                    binding.translateView.isFocusableInTouchMode = false
-                    binding.editButton.text = "Start editing word"
-                    viewModel.wordEdited()
-                }
+        viewModel.navigateToEdit.observe(viewLifecycleOwner, Observer { selectedId ->
+            if (selectedId > 0) {
+                findNavController().navigate(LearningFragmentDirections.actionLearningToEditFragment(selectedId))
+                viewModel.navigatedToEditWord()
             }
         })
 
         return binding.root
     }
-
 }
--- app/src/main/java/com/cloudsheeptech/vocabulary/learning/LearningViewModel.kt
+++ app/src/main/java/com/cloudsheeptech/vocabulary/learning/LearningViewModel.kt
@@ -5,7 +5,6 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.cloudsheeptech.vocabulary.SingleEvent
-import com.cloudsheeptech.vocabulary.data.Confidence
 import com.cloudsheeptech.vocabulary.data.LearnWord
 import com.cloudsheeptech.vocabulary.data.Vocabulary
 import com.cloudsheeptech.vocabulary.data.Word
@@ -30,24 +29,11 @@ class LearningViewModel(val vocabulary: Vocabulary) : ViewModel() {
     val progress : LiveData<String>
         get() = _progress
 
-    private var _internalEditToggle = false
-    private var _internalEditAbort = false
-
-    private val _editToggle = MutableLiveData<SingleEvent<Boolean>>()
-    val editToggle : LiveData<SingleEvent<Boolean>>
-        get() = _editToggle
-
-    private val _editedToggle = MutableLiveData<SingleEvent<Boolean>>()
-    val editedToggle : LiveData<SingleEvent<Boolean>>
-        get() = _editedToggle
+    private val _navigateToEdit = MutableLiveData<Int>(-1)
+    val navigateToEdit : LiveData<Int> get() = _navigateToEdit
 
     private var currVocabIdx = 0
     private var learningList = LearningStack()
-
-    // Navigation
-    private val _navigateToAddWord = MutableLiveData<Boolean>(false)
-    val navigateToWord : LiveData<Boolean> get() = _navigateToAddWord
-    // ----
 
     init {
         learningVocabulary.value = "Click on next"
@@ -58,82 +44,42 @@ class LearningViewModel(val vocabulary: Vocabulary) : ViewModel() {
 //        }
     }
 
-    fun requestVocabulary() {
-        vmScope.launch {
-            vocabulary.updateVocabulary()
-            // Modifies data in this class, cannot run on background thread
-            withContext(Dispatchers.Main) {
-                for (word in vocabulary.vocabulary) {
-                    learningList.addWord(LearnWord(word))
-                }
-                showNextWord()
-            }
-        }
-    }
-
     fun showNextWord() {
         Log.i("LearningViewModel", "pressed next word")
-        // Check if current word is edited and abort in that case edit
-        if (_internalEditToggle) {
-            _internalEditAbort = true
-            editWord()
-        }
+
         var next = Word(ID = 0, Vocabulary = "Empty", Translation = "Empty")
         if (learningList.isNotEmpty()) {
             val tmp = learningList.getNextWord()
             if (tmp != null) {
                 next = tmp.word
+                currVocabIdx = next.ID
             }
+        } else {
+            for (word in vocabulary.wordList) {
+                learningList.addWord(LearnWord(word))
+            }
+            showNextWord()
         }
         this.learningVocabulary.value = next.Vocabulary
         this.translateVocabulary.value = next.Translation
-        this._progress.value = (currVocabIdx + 1).toString() + "/" + vocabulary.vocabulary.size.toString()
+        this._progress.value = (currVocabIdx + 1).toString() + "/" + vocabulary.wordList.size.toString()
         Log.i("LearningViewModel", "Updated vocab to: ${learningVocabulary.value}")
     }
 
     fun removeWord() {
-        val removeIdx = Math.floorMod(currVocabIdx-1, vocabulary.vocabulary.size)
-        Log.i("LearningViewModel", "Removing word at $removeIdx - $currVocabIdx - ${vocabulary.vocabulary.size}")
+        val removeIdx = Math.floorMod(currVocabIdx, vocabulary.wordList.size)
+        Log.i("LearningViewModel", "Removing word at $removeIdx - $currVocabIdx - ${vocabulary.wordList.size}")
         vmScope.launch {
             vocabulary.removeVocabularyItem(removeIdx)
         }
     }
 
     fun editWord() {
-        Log.i("LearningViewModel", "Switched editing vocab")
-        if (_internalEditToggle) {
-            _internalEditToggle = false
-            _editedToggle.value = SingleEvent<Boolean>(true)
-        } else {
-            _internalEditToggle = true
-            _editToggle.value = SingleEvent<Boolean>(true)
-        }
+        _navigateToEdit.value = currVocabIdx
     }
 
-    fun wordEdited() {
-        Log.i("LearningViewModel", "Word edited")
-        if (learningVocabulary.value == null || learningVocabulary.value == null) {
-            Log.i("LearningViewModel", "One of the two values is empty")
-            return
-        }
-        if (_internalEditAbort) {
-            Log.i("LearningViewModel", "Aborting word edit")
-            _internalEditAbort = false
-            return
-        }
-        val updatedIdx = Math.floorMod(currVocabIdx-1, vocabulary.vocabulary.size)
-        val updatedWord = Word(updatedIdx, learningVocabulary.value!!, translateVocabulary.value!!)
-        Log.i("LearningViewModel", "Updating word to: $updatedWord")
-        vmScope.launch {
-            vocabulary.modifyVocabularyItem(updatedWord)
-        }
+    fun navigatedToEditWord() {
+        _navigateToEdit.value = -1
     }
 
-    fun navigateToAddWord() {
-        _navigateToAddWord.value = true
-    }
-
-    fun onAddWordNavigated() {
-        _navigateToAddWord.value = false
-    }
 }

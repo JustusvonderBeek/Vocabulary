4fbf81fbcbe4aa5da7fbea895f44477de3934710
Justus <beek@in.tum.de>
2023-10-16 19:29:55 +0200

    Moved

--- app/src/main/java/com/cloudsheeptech/vocabulary/data/SwipeToDeleteHandler.kt
+++ app/src/main/java/com/cloudsheeptech/vocabulary/data/SwipeToDeleteHandler.kt
@@ -2,7 +2,7 @@ package com.cloudsheeptech.vocabulary.data
 
 import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.RecyclerView
-import com.cloudsheeptech.vocabulary.edit.WordListItemAdapter
+import com.cloudsheeptech.vocabulary.editlist.WordListItemAdapter
 
 class SwipeToDeleteHandler(private val adapter: WordListItemAdapter) : ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.LEFT) {
     override fun onMove(
@@ -14,7 +14,7 @@ class SwipeToDeleteHandler(private val adapter: WordListItemAdapter) : ItemTouch
     }
 
     override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
-        adapter.deleteItemAt(viewHolder.adapterPosition)
+//        adapter.deleteItemAt(viewHolder.adapterPosition)
     }
 
 }

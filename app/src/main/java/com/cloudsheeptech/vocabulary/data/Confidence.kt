dbda343c4889a0c29d94edf7eba16463d65ff40c
Justus von der Beek <justusvonderbeek@gmail.com>
2023-10-19 16:18:17 +0200

    Added description for levels

--- app/src/main/java/com/cloudsheeptech/vocabulary/data/Confidence.kt
+++ app/src/main/java/com/cloudsheeptech/vocabulary/data/Confidence.kt
@@ -1,9 +1,9 @@
 package com.cloudsheeptech.vocabulary.data
 
 enum class Confidence {
-    PERFECT,
-    GOOD,
-    BAD,
-    POOR,
-    NEW
+    PERFECT,    // Bigger 95 - just repeat long term
+    GOOD,       // Bigger 75 - not perfect yet, repeat regularly to make them stick
+    BAD,        // Bigger 50 - more or less random, only a few per lesson but focused until good
+    POOR,       // Less 30 - often mistakes, very hard, should be included in every lesson
+    NEW         // 0 - should be added if all the others are not too bad, new words
 }

1a19c38d891615e09d3316ef992f6b0b793f9a8b
Justus von der Beek <justusvonderbeek@gmail.com>
2023-10-20 14:01:57 +0200

    Added comparable

--- app/src/main/java/com/cloudsheeptech/vocabulary/data/LearnWord.kt
+++ app/src/main/java/com/cloudsheeptech/vocabulary/data/LearnWord.kt
@@ -4,4 +4,12 @@ data class LearnWord(
     var word : Word,
     var confidence: Confidence = Confidence.NEW,
     var repeat: Int = 0,
-)
+) : Comparable<LearnWord> {
+    override fun compareTo(other: LearnWord): Int {
+        return if (this.confidence.ordinal != other.confidence.ordinal)
+            this.confidence.ordinal - other.confidence.ordinal
+        else
+            this.repeat - other.repeat
+    }
+
+}

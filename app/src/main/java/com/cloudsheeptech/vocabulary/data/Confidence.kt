c1d36b708d0570c182408355871356eeb95ab891
Justus von der Beek <justusvonderbeek@gmail.com>
2023-10-20 14:01:10 +0200

    Updated order and value assignment

--- app/src/main/java/com/cloudsheeptech/vocabulary/data/Confidence.kt
+++ app/src/main/java/com/cloudsheeptech/vocabulary/data/Confidence.kt
@@ -1,9 +1,14 @@
 package com.cloudsheeptech.vocabulary.data
 
-enum class Confidence {
-    PERFECT,    // Bigger 95 - just repeat long term
-    GOOD,       // Bigger 75 - not perfect yet, repeat regularly to make them stick
-    BAD,        // Bigger 50 - more or less random, only a few per lesson but focused until good
-    POOR,       // Less 30 - often mistakes, very hard, should be included in every lesson
-    NEW         // 0 - should be added if all the others are not too bad, new words
+enum class Confidence(val value : Int) {
+    NEW(0),         // 0 - should be added if all the others are not too bad, new words
+    POOR(1),       // Less 30 - often mistakes, very hard, should be included in every lesson
+    BAD(2),        // Bigger 50 - more or less random, only a few per lesson but focused until good
+    GOOD(3),       // Bigger 75 - not perfect yet, repeat regularly to make them stick
+    PERFECT(4);    // Bigger 95 - just repeat long term
+
+    companion object {
+        fun fromInt(value: Int) = Confidence.values().first { it.value == value }
+    }
+
 }

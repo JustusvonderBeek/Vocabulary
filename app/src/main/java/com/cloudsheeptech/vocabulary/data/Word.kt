58dd22993823814d6e617414c69133675c5fa798
Justus von der Beek <justusvonderbeek@gmail.com>
2023-10-20 15:35:34 +0200

    Started including confidence values

--- app/src/main/java/com/cloudsheeptech/vocabulary/data/Word.kt
+++ app/src/main/java/com/cloudsheeptech/vocabulary/data/Word.kt
@@ -6,5 +6,14 @@ import kotlinx.serialization.Serializable
 data class Word(
     val ID : Int,
     val Vocabulary : String,
-    val Translation : String
-)
+    val Translation : String,
+    val Confidence : Confidence = com.cloudsheeptech.vocabulary.data.Confidence.NEW,
+    val Repeat : Int = 0
+) : Comparable<Word> {
+    override fun compareTo(other: Word): Int {
+        return if (this.Confidence.ordinal != other.Confidence.ordinal)
+            this.Confidence.ordinal - other.Confidence.ordinal
+        else
+            this.Repeat - other.Repeat
+    }
+}
